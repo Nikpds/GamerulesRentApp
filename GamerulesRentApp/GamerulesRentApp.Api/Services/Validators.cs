d176de77b30a7242e128ff5df6d1f08b761a817d
nperperidis <npds86@gmail.com>
2018-05-27 12:57:11 +0300

    asda

---
+++ GamerulesRentApp/GamerulesRentApp.Api/Services/Validators.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamerulesRentApp.Api.Services
+{
+    public class Validators
+    {
+    }
+}
